dabe4cc104470d45362a9fbde2153c38bd4b2bd7
Marcus Gutierrez <marcusgttrz@gmail.com>
2020-05-25 23:27:05 +0000

    Main cleanup

--- main.py
+++ main.py
@@ -5,28 +5,22 @@ from vm_manager import vmManager
 from database import Database
 from ssh_keystore import EchKeystore
 from instance_definitions import Instance
-#from guest_image import GuestImage
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
+# Basic user account for testing
 user = {
     "account_id": "12345",
     "account_user_id": "11119",
 }
 
 vmHost = vmManager()
-print("--all--instances---")
-instances = vmHost.getAllInstances(user)
-print(json.dumps(instances, indent=4))
 
-print("--specific--instance---")
-results = vmHost.getInstanceMetaData(user, "vm-a8b30fda")
-print(results)
-
+# Example Instance definition
 instanceType = Instance("standard", "small")
 
-
+# Example ssh key definition
 key_meta = EchKeystore().get_key(user, "test_key")
 
 cloudinit_params = {
@@ -37,7 +31,6 @@ cloudinit_params = {
     "gateway_ip": "172.16.9.1"
 }
 server_params = {
-    # ubuntu-18.04-server-cloudimg-amd64.img
     "image_id": "gmi-fc1c9a62",
     #"vmi": "vmi-293de.qcow2",
     "disk_size": "10G",
