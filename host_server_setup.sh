226158883cb4a105323d459df8e3cfd77161ad88
Marcus Gutierrez <marcusgttrz@gmail.com>
2020-06-01 23:49:55 +0000

    VM-Windows-Additions Not yet implemented

--- host_server_setup.sh
+++ host_server_setup.sh
@@ -3,20 +3,8 @@ virt-host-validate qemu
 virt-host-validate lxc
 
 # Ubuntu 18.04.4 LTS
-sudo apt install libguestfs-tools jq
+sudo apt-get install qemu-kvm libvirt-bin virtinst bridge-utils cpu-checker 
+sudo apt install libguestfs-tools jq ovmf
 
 # This application to be installed in:
 /opt/echome/
-
-
-# https://mathiashueber.com/configuring-hugepages-use-virtual-machine/
-# for Windows VMs
-# If error, install
-hugeadm --explain
-sudo apt install hugepages
-# follow the article to configure, add to your Xml:
-  <currentMemory unit='MB'>$VM_MEMORY</currentMemory>
-  <memoryBacking>    
-    <hugepages/>  
-  </memoryBacking>
-  <vcpu>$VM_CPU_COUNT</vcpu>
