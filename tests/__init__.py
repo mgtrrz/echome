8339d225d7df2adf25dfb4157c3889f1e1b138a7
Marcus Gutierrez <marcusgttrz@gmail.com>
2020-06-02 04:36:34 +0000

    non-working testing framework

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+from test_vm_manager import TestVmManager
--- tests/test_vm_manager.py
+++ tests/test_vm_manager.py
@@ -1,13 +1,29 @@
 import unittest
 
+class TestVmManager(unittest.TestCase):
 
-class TestSum(unittest.TestCase):
-
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+    def test_generate_network_cloudinit_config(self):
+        expected = """version: 2
+ethernets:
+  ens2:
+    dhcp4: false
+    dhcp6: false
+    addresses:
+    - 172.16.9.50/24
+    gateway4: 172.16.9.1
+    nameservers:
+      addresses:
+      - 1.1.1.1
+      - 1.0.0.1
+"""     
+        config = {
+            "network_type": "BridgetoLan",
+            "private_ip": "172.16.9.50/24",
+            "gateway_ip": "172.16.9.1"
+        }
+        m = vm_manager.vmManager()
+        actual = m._VmManager__generate_cloudinit_config(config)
+        self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
     unittest.main()
