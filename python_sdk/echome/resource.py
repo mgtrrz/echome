e974862991088796fb07dcfda5f45daf720eae58
Marcus Gutierrez <marcusgttrz@gmail.com>
2020-07-01 02:30:59 +0000

    Python SDK has moved to dedicated repo

--- python_sdk/echome/resource.py
+++
@@ -1,8 +0,0 @@
-# import sys
-# import requests
-# from .session import Session
-# from .vm import vm
-
-# def resource(type, session: Session):
-#     req_resource = getattr(sys.modules[__name__], type)
-#     return req_resource(session)
