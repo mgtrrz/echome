90e353d7f5f6b995e18e17bf05b07ff932547d2a
Marcus Gutierrez <markg90@gmail.com>
2021-08-29 13:22:33 -0500

    Created accounts

--- echome/identity/models.py
+++ echome/identity/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from echome.id_gen import IdGenerator
 
 class AttemptedOverrideOfAccountIdException(Exception):
+    pass
+
+class AccountNotFound(Exception):
     pass
 
 class Account(models.Model):
@@ -22,13 +25,52 @@ class Account(models.Model):
         else:
             raise AttemptedOverrideOfAccountIdException
 
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, password=None, account=None):
+        acct = Account.objects.get(account_id=account)
+        print(acct)
+        if not acct:
+            raise AccountNotFound
+
+        user = self.model()
+        user.set_password(password)
+        user.account = acct
+        user.username = username
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, password, account):
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(username, password, account)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
 
 class User(AbstractUser):
     user_id = models.CharField(max_length=20, unique=True)
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, to_field="account_id")
     created = models.DateTimeField(auto_now_add=True, null=False)
-    secret = models.TextField()
+    secret = models.TextField(null=True)
     tags = models.JSONField(null=True)
+
+    objects = UserManager()
+
+    REQUIRED_FIELDS = [
+        'account'
+    ]
+
+    def set_account(self, account_id):
+        acct = Account(account_id=account_id)
+        if acct:
+            self.account = acct.id
+        else:
+            raise AccountNotFound
 
     def __str__(self) -> str:
         return self.user_id
@@ -38,7 +80,7 @@ class UserAccessAccounts(AbstractBaseUser):
     auth_id = models.CharField(max_length=20, unique=True)
     parent_user = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True, null=False)
-    secret = models.TextField()
+    secret = models.TextField(null=True)
     tags = models.JSONField(null=True)
 
     def __str__(self) -> str:
