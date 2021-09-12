125a984c4170944b06e47a16588a736a0d0413f6
Marcus Gutierrez <markg90@gmail.com>
2021-09-12 14:17:38 -0500

    image manager moved to model

--- echome/images/models.py
+++ echome/images/models.py
@@ -1,6 +1,20 @@
+import os
+import logging
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+from identity.models import User
+from echome.id_gen import IdGenerator
+from echome.commander import QemuImg
+from echome.exceptions import AttemptedOverrideOfImmutableIdException
 
+logger = logging.getLogger(__name__)
+
+# All images (disk images) derive from this model.
+# There's currently only two types:
+# GuestImage (gmi-): For ALL accounts/users on the server
+# UserImage (vmi-): For only a specific account on the server
 class BaseImageModel(models.Model):
+    image_type = None
     image_id = models.CharField(max_length=20, unique=True, db_index=True)
     created = models.DateTimeField(auto_now_add=True, null=False)
     last_modified = models.DateTimeField(auto_now=True)
@@ -18,16 +32,85 @@ class BaseImageModel(models.Model):
     class Meta:
         abstract = True
 
+    def generate_id(self):
+        if self.image_id is None or self.image_id == "":
+            if self.img_type == "guest":
+                id = IdGenerator.generate(type="gmi")
+            elif self.img_type == "user":
+                id = IdGenerator.generate(type="vmi")
+            else:
+                raise Exception("Unknown image type provided")
+
+            self.image_id = IdGenerator.generate(id)
+        else:
+            raise AttemptedOverrideOfImmutableIdException
+
+    def register_image(self, path:str, name:str, description:str, user:User=None, host="localhost", tags=dict):
+        # Check to see if a file exists at the provided path
+        if not os.path.exists(path):
+            logger.error(f"File does not exist at specified file path: {path}")
+            raise InvalidImagePath(f"File does not exist at specified file path: {path}")
+
+        self.generate_id()
+        
+        # Check to see if an image at the path already exists
+        try:
+            results = self.objects.get(
+                image_path=path
+            )
+        except ObjectDoesNotExist:
+            pass
+        else:
+            logger.error(f"Image already exists in database. img_path={path}")
+            raise InvalidImageAlreadyExists(f"Image already exists in database. img_path={path}")
+
+
+        # Verify image type
+        obj = QemuImg().info(path)
+        print(obj["format"])
+
+        img_metadata = {}
+        img_metadata["user"] = user.user_id
+        img_metadata["format"] = obj["format"]
+        img_metadata["actual-size"] = obj["actual-size"]
+        img_metadata["virtual-size"] = obj["virtual-size"]
+
+        if self.image_type == "user":
+            self.account = user.account
+        
+        self.image_path = path
+        self.name = name
+        self.description = description
+        #self.minimum_requirements = dict
+        self.image_metadata = img_metadata
+        self.tags = tags
+        self.save
+        return self.image_id
+
     def __str__(self) -> str:
         return self.image_id
 
 class GuestImage(BaseImageModel):
+    image_type = "guest"
     def __str__(self) -> str:
         return self.image_id
     
 
 class UserImage(BaseImageModel):
+    image_type = "user"
     account = models.ForeignKey("identity.Account", on_delete=models.CASCADE, to_field="account_id", null=True)
 
     def __str__(self) -> str:
         return self.image_id
+
+class InvalidImageId(Exception):
+    pass
+
+class InvalidImagePath(Exception):
+    pass
+
+class InvalidImageAlreadyExists(Exception):
+    pass
+
+class UserImageInvalidUser(Exception):
+    pass
