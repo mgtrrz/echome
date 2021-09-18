a742b3ec6e721154a1e0e5c44450b0601cfc334a
Marcus Gutierrez <marcusgttrz@gmail.com>
2021-09-18 15:52:11 +0000

    Clean up

--- echome-legacy/backend/tests/id_gen_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from ../id_gen import id_gen
-
-class NamesTestCase(unittest.TestCase):
-
-   def test_first_last_name(self):
-       result = formatted_name("pete", "seeger")
-       self.assertEqual(result, "Pete Seeger")
