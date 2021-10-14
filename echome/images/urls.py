6ab2bf97602a03165c6364884a84b8bec002c93c
Marcus Gutierrez <marcusgttrz@gmail.com>
2021-10-14 01:10:38 +0000

    Images

--- echome/images/urls.py
+++ echome/images/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import *
+from .views import CreateImage, DeleteImage, DescribeImage, ModifyImage
 
 app_name = 'images'
 urlpatterns = [
-    # path('create', CreateNetwork.as_view()),
-    # path('describe/<str:net_id>', DescribeNetwork.as_view()),
-    # path('terminate/<str:net_id>', TerminateNetwork.as_view()),
-    # path('modify/<str:net_id>', ModifyNetwork.as_view()),
+    path('create', CreateImage.as_view()),
+    path('<str:type>/describe/<str:image_id>', DescribeImage.as_view()),
+    path('<str:type>/delete/<str:image_id>', DeleteImage.as_view()),
+    path('<str:type>/modify/<str:image_id>', ModifyImage.as_view()),
 ]
--- echome/images/views.py
+++ echome/images/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+import logging
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework import status
+from api.api_view import HelperView
+from .serializers import GuestImageSerializer, UserImageSerializer
+from .models import GuestImage, UserImage
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
+class CreateImage(HelperView, APIView):
+    pass
+
+
+class DeleteImage(HelperView, APIView):
+    pass  
+
+
+class DescribeImage(HelperView, APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, type:str, image_id:str):
+        i = []
+
+        if type not in ["guest", "user"]:
+            return self.error_response(
+                "Unknown type",
+                status = status.HTTP_404_NOT_FOUND
+            )
+        
+        if type == "guest":
+            try:
+                if image_id == "all":
+                    images = GuestImage.objects.all()
+                else:
+                    images = []
+                    images.append(GuestImage.objects.get(
+                        image_id=image_id
+                    ))
+                
+                for image in images:
+                    i.append(GuestImageSerializer(image).data)
+            except GuestImage.DoesNotExist as e:
+                logger.debug(e)
+                return self.not_found_response()
+            except Exception as e:
+                logger.exception(e)
+                return self.internal_server_error_response()
+
+        return self.success_response(i)
+
+
+class ModifyImage(HelperView, APIView):
+    pass
